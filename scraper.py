04cd9f698bda9e11be41245fe49cb8e0883bde8d
tfarkas <timothy.farkas@gmail.com>
2022-08-13 14:24:43 -0600

    get to dataframe for senators

--- scraper.py
+++ scraper.py
@@ -1,21 +1,43 @@
 import requests 
 from bs4 import BeautifulSoup
+import pandas as pd
 
-URL = "https://nmlegis.gov/Members/Legislator?SponCode=SBACA"
-r = requests.get(URL)
+senate_url = "https://nmlegis.gov/Members/Legislator_List?T=S"
+r_senate = requests.get(senate_url)
+senate_soup = BeautifulSoup(r_senate.content, 'html5lib')
+senate_list = senate_soup.find('select', attrs = {'id':'MainContent_ddlLegislators'})
+rep_names = []
+rep_tags = []
+for rep in senate_list.findAll('option'):
+     rep_tags.append(rep['value'])
+     rep_names.append(rep.text)
+#    print(rep['value'])
+rep_names.pop(0)
+rep_tags.pop(0)
+print(rep_names)
 
-soup = BeautifulSoup(r.content, 'html5lib') # If this line causes an error, run 'pip install html5lib' or install html5lib
-#print(soup.prettify())
+dictlist = []
+for name, rep in zip(rep_names, rep_tags):
+    URL = f"https://nmlegis.gov/Members/Legislator?SponCode={rep}"
+    r = requests.get(URL)
+    soup = BeautifulSoup(r.content, 'html5lib') 
+    #print(soup.prettify())
+    full_name = soup.find('span', attrs = {'id':'MainContent_formViewLegislatorName_lblLegislatorName'})
+    party = full_name.text[(full_name.text.find('(') + 1):full_name.text.find(')')]
+    info = soup.find('div', attrs = {'class':'col-xs-12 col-sm-9 legislator-information'}) 
+    out_dict = {'Legislator':name, 'Party':party}
+    for row in info.findAll('li', attrs = {'class':'list-group-item'}):
+        attr = row.b.text
+        attr = attr.replace(':', '')
+        try: 
+            datum = row.span.text
+        except: 
+            datum = row.a.text
+        dict_add = {attr:datum}
+        out_dict.update(dict_add)
+    #print(out_dict)
+    dictlist.append(out_dict)
 
-info = soup.find('div', attrs = {'class':'col-xs-12 col-sm-9 legislator-information'}) 
-out_dict = dict()
-for row in info.findAll('li', attrs = {'class':'list-group-item'}):
-    attr = row.b.text
-    attr = attr.replace(':', '')
-    try: 
-        datum = row.span.text
-    except: 
-        datum = row.a.text
-    dict_add = {attr:datum}
-    out_dict.update(dict_add)
-print(out_dict)
+#print(dictlist)
+df = pd.DataFrame(dictlist)
+print(df)
